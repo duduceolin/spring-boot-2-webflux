82e4aa1fa525c2a1b56e0815c1304e32cafd9064
Eduardo Ceolin <eduardo_ceolin@terceiros.sicredi.com.br>
2018-05-23 22:32:21 -0300

    Criação aplicação spring boot

--- src/main/java/io/ceolin/ms/Application.java
+++ src/main/java/io/ceolin/ms/Application.java
@@ -1,5 +1,15 @@
 package io.ceolin.ms;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@SpringBootApplication
+@EnableReactiveMongoRepositories
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
